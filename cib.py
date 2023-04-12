56490a4d0176e1f95e1ba2a21d60277be9ae81e3
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-04-12 21:32:50 +0330

    update cib.py

---
+++ cib.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import getopt
+import subprocess
+from colorama import Fore
+
+BUILD_DIR = "build"
+TEST_BIN_DIR = "build/test"
+TEST_BIN_FILE = "test_sort"
+COMPILER_TIMEOUT = 10.0
+
+ERROR = 1
+SUCCESS = 0
+
+FLAG_BUILD = False
+FLAG_REBUILD = False
+FLAG_TEST = False
+FLAG_CLEAN = False
+
+
+def usage():
+    print("\nUSAGE:")
+    print("python3", sys.argv[0], "< -short-option > | < --long-option >")
+
+    print("\nOPTIONS:")
+    print("python3", sys.argv[0], "<-h | --help>   # print usage")
+    print("python3", sys.argv[0], "<-c | --clean>  # remove build folder")
+    print("python3", sys.argv[0], "<-b | --build>  # build source files")
+    print("python3", sys.argv[0], "<-t | --test>   # run unit tests")
+    print("python3", sys.argv[0],
+          "<-a | --all>    # rebuild source files and run unit tests ")
+
+    print("\nEXAMPLE:")
+    print("python3", sys.argv[0], "-c   or   ",
+          "python3", sys.argv[0], "--clean")
+    print("python3", sys.argv[0], "-r   or   ",
+          "python3", sys.argv[0], "--rebuild")
+    print("python3", sys.argv[0], "-b   or   ",
+          "python3", sys.argv[0], "--build")
+    print("python3", sys.argv[0], "-t   or   ",
+          "python3", sys.argv[0], "--test")
+    print("python3", sys.argv[0], "-a   or   ",
+          "python3", sys.argv[0], "--all")
+
+# check status of return
+
+
+def assertReturn(ret):
+    output = ret.stdout.decode("utf-8")
+    print(output)
+    output = ret.stderr.decode("utf-8")
+    print(output)
+
+    if ret.returncode != 0:
+        print(Fore.RED+"PROCCESS FAILED.\n"+Fore.RESET)
+        exit(ERROR)
+
+    print(Fore.GREEN+"PROCCESS SUCCEEDED.\n"+Fore.RESET)
+
+
+def executeCommond(cmd):
+    try:
+        ret = subprocess.run(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+    except Exception as e:
+        print(Fore.RED+"ERROR: EXCUTION OF CMD FAILED."+Fore.RESET, str(e))
+        exit(ERROR)
+
+    return ret
+
+
+# configure and build source files
+def build():
+
+    print(Fore.BLUE+"START CONFIGURATION ...\n"+Fore.RESET)
+    ret = executeCommond(["cmake", "-S", ".", "-B", BUILD_DIR])
+    assertReturn(ret)
+
+    print(Fore.BLUE+"START BUILDING ...\n"+Fore.RESET)
+    ret = executeCommond(["cmake", "--build", BUILD_DIR])
+    assertReturn(ret)
+
+
+def updateSubmodule():
+    print(Fore.BLUE+"UPDATING SUBMODULES ..."+Fore.RESET)
+    ret = executeCommond(
+        ["git", "submodule", "update", "--init", "--recursive"])
+    assertReturn(ret)
+
+
+def runUnitTest():
+
+    print(Fore.BLUE+"START TESTING ...\n"+Fore.RESET)
+    utb_path = os.path.join(TEST_BIN_DIR, TEST_BIN_FILE)
+    ret = executeCommond([utb_path])
+    output = ret.stdout.decode("utf-8")
+    print("RUBTIME_OUTPUT:\n", output)
+
+
+def cleanBuild():
+    print(Fore.BLUE+"REMOVING BUILD ...\n"+Fore.RESET)
+    ret = executeCommond(["rm", "-rf", BUILD_DIR])
+    assertReturn(ret)
+
+
+def getOptions(argv):
+
+    opts, _ = getopt.getopt(
+        argv, "hcbrta", ["help", "clean", "build", "rebuild", "test", "all"])
+
+    if opts == []:
+        return False
+
+    global FLAG_BUILD, FLAG_TEST, FLAG_REBUILD, FLAG_CLEAN
+
+    for opt, _ in opts:
+
+        if opt in ("-r", "--rebuild"):
+            FLAG_CLEAN = True
+            FLAG_BUILD = True
+
+        elif opt in ("-b", "--build"):
+            FLAG_BUILD = True
+
+        elif opt in ("-t", "--test"):
+            FLAG_TEST = True
+
+        elif opt in ("-a", "--all"):
+            FLAG_CLEAN = True
+            FLAG_BUILD = True
+            FLAG_TEST = True
+
+        elif opt in ("-c", "--clean"):
+            FLAG_CLEAN = True
+
+        elif opt in ("-h", "--help"):
+            usage()
+            exit(SUCCESS)
+
+        else:
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+
+    if getOptions(sys.argv[1:]) == False:
+        usage()
+        exit(ERROR)
+
+    if FLAG_CLEAN:
+        cleanBuild()
+
+    if FLAG_BUILD:
+        updateSubmodule()
+        build()
+
+    if FLAG_TEST:
+        runUnitTest()
+
+    print(Fore.GREEN+"ALL TEST PASSED."+Fore.RESET)
+    exit(SUCCESS)
